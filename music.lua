50f9d2bbfbf1f0ff8460063a1aaa530bffb6af3d
Polo <jpolo5678@gmail.com>
2025-09-19 00:04:37 -0400

    support album art

--- music.lua
+++ music.lua
@@ -1,16 +1,18 @@
 local lgi = require("lgi")
 local Playerctl = lgi.require("Playerctl")
+local GLib = lgi.require("GLib")
+local GdkPixbuf = lgi.require("GdkPixbuf")
 
 local M = {}
 
 function M.player_info()
   player = Playerctl.Player.new()
   return {
-  	artist     = player:print_metadata_prop("xesam:artist"),
-  	title      = player:print_metadata_prop("xesam:title"),
-  	album      = player:print_metadata_prop("xesam:album"),
-  	artUrl     = player:print_metadata_prop("mpris:artUrl"),
-  	length     = player:print_metadata_prop("mpris:length")
+  	artist = player:print_metadata_prop("xesam:artist"),
+  	title  = player:print_metadata_prop("xesam:title"),
+  	album  = player:print_metadata_prop("xesam:album"),
+  	artUrl = player:print_metadata_prop("mpris:artUrl"),
+  	length = player:print_metadata_prop("mpris:length")
  }
 end
 
@@ -63,6 +65,30 @@ end
 function M.previous()
   player = Playerctl.Player.new()
   return player:previous()
+end
+
+function M.get_album_art()
+  -- para los usarios de awesomewm es necesario cargar la superficie cairo desde pixbuf
+  local info = M.player_info()
+  local artUrlstr = info.artUrl
+
+  if artUrlstr and string.find(artUrlstr, "^data:image/.*;base64,") then
+    image64 = string.gsub(artUrlstr, "^data:image/[^;]*;base64,", "")
+    decode_image = GLib.base64_decode(image64)
+    if decode_image then
+      local imagePix = GdkPixbuf.PixbufLoader.new()
+      imagePix:write(decode_image)
+      imagePix:close()
+      return imagePix:get_pixbuf()
+    end
+  end
+
+  if artUrlstr and string.find(artUrlstr, "^file://") then
+    local file_path = string.gsub(artUrlstr, "^file://", "")
+    file_path = GLib.uri_unescape_string(file_path, nil)
+    return GdkPixbuf.Pixbuf.new_from_file(file_path)
+  end
+      
 end
 
 return M
--- prueba.lua
+++ prueba.lua
@@ -4,43 +4,11 @@ local GLib = lgi.require("GLib", "2.0")
 local GdkPixbuf = lgi.require("GdkPixbuf", "2.0")
 local Playerctl = lgi.require("Playerctl")
 local Gio = lgi.Gio
+local music = require("music")
 
 local appID = "io.github.poloz_z.GTK-test.Lua.Gtk4.Image"
 local app_tile = "GtkImageBS64"
 local app = Gtk.Application.new(appID, Gio.ApplicationFlags.FLAGS_NONE)
-
-local function update_metadata(image, title_label, artist_label)
-    local c = Playerctl.Player.new()
-    
-    -- Obtener metadatos
-    local image64str = c:print_metadata_prop("mpris:artUrl")
-    local title = c:print_metadata_prop("xesam:title")
-    local artist = c:print_metadata_prop("xesam:artist")
-    
-    -- Actualizar labels
-    title_label:set_text(title or "sin titulo")
-    artist_label:set_text(artist or "sin artista")
-    
-    if image64str then
-        if string.find(image64str, "^data:image/.*;base64,") then
-            local image64 = string.gsub(image64str, "^data:image/[^;]*;base64,", "")
-            local decode_image = GLib.base64_decode(image64)
-            if decode_image then
-                local loader = GdkPixbuf.PixbufLoader.new()
-                loader:write(decode_image)
-                loader:close()
-                image:set_pixbuf(loader:get_pixbuf())
-            end
-        elseif string.find(image64str, "^file://") then
-            local file_path = string.gsub(image64str, "^file://", "")
-            file_path = GLib.uri_unescape_string(file_path, nil)
-            local pixbuf = GdkPixbuf.Pixbuf.new_from_file(file_path)
-            image:set_pixbuf(pixbuf)
-        end
-    end
-    
-    return true
-end
 
 function app:on_startup()
     local window = Gtk.ApplicationWindow.new(self)
@@ -54,16 +22,13 @@ function app:on_startup()
     local title_label = Gtk.Label.new("Cargando...")
     local artist_label = Gtk.Label.new("Cargando...")
     local image = Gtk.Picture.new()
+    image:set_pixbuf(music.get_album_art())
 
     box:append(title_label)
     box:append(artist_label)
     box:append(image)
     box.valign = Gtk.Align.CENTER
 
-    -- Actualizar cada segundo
-    GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, function()
-        return update_metadata(image, title_label, artist_label)
-    end)
 end
 
 function app:on_activate()
